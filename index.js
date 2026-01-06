ce7fcfed3265688c2370cf8dd183242aa49e1e3f
Fernando Lopez <frlopez88@icloud.com>
2026-01-05 19:43:21 -0600

    AI integration!

--- index.js
+++ index.js
@@ -9,20 +9,41 @@ const apiModel = process.env.API_CLOUD_FARE_MODEL;
 
 let info = "";
 
+let messages = [
+    {
+        role:"system",
+        content:`You are going to be a AI Agent that will be solving question abou
+        1) The Batman Movie, the one from 2021.
+        2) The Superman vs Batman movie from zack snyder
+        3) The 3 Batman movies from Christopher Nolan `
+    }
+];
+
 while (info != "exit"){
 
-    info = input("Enter anything: ")
-    console.log(info)
+    info = input("Ask me anything about The Batman Movies since 2005 up: ")
+    
+    const msg = {
+        role: "user",
+        content : info
+    };
+
+    messages.push(msg);
+
+    const result = await run(apiModel, messages);
+
+    console.log(result.result.response);
 
 }
 
-async function run(model, input) {
+async function run(model, msg) {
+    
   const response = await fetch(
-    `https://api.cloudflare.com/client/v4/accounts/d0fd23ef4f9eb62ba0059edeb492598b/ai/run/${model}`,
+    `https://api.cloudflare.com/client/v4/accounts/${apiAccount}/ai/run/${model}`,
     {
-      headers: { Authorization: "Bearer JFGxGzu1rddMfuycIFc0969zeweIaSzfcFAENhAj" },
+      headers: { Authorization: `Bearer ${apiToken}` },
       method: "POST",
-      body: JSON.stringify(input),
+      body: JSON.stringify({messages: msg}),
     }
   );
   const result = await response.json();
